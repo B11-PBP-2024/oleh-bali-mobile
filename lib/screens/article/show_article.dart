0f56fedcbfbae7b870e0280592259a540aa74351
Ezar Surahman <akhdanezar@gmail.com>
2024-12-01 18:42:43 +0700

    Add functionality to show, edit, create, and delete Article

---
+++ lib/screens/article/show_article.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'package:oleh_bali_mobile/models/article_entry.dart';
+import 'package:oleh_bali_mobile/screens/article/add_article.dart';
+import 'package:oleh_bali_mobile/widgets/article/article_card.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+
+class ShowArticle extends StatefulWidget {
+  const ShowArticle({super.key});
+
+  @override
+  State<ShowArticle> createState() => _ShowArticleState();
+}
+
+class _ShowArticleState extends State<ShowArticle> {
+  bool showAllArticles = true;
+  List<ArticleEntry> articles = [];
+  late Future<void> _fetchArticlesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchArticlesFuture = fetchArticles(context.read<CookieRequest>());
+  }
+
+  Future<void> fetchArticles(CookieRequest request) async {
+    final response = await request.get(showAllArticles
+        ? "http://localhost:8000/article/json-all/"
+        : "http://localhost:8000/article/json-user/");
+    var data = response;
+    List<ArticleEntry> fetchedArticles = [];
+    for (var d in data) {
+      if (d != null) {
+        ArticleEntry article = ArticleEntry.fromJson(d);
+        fetchedArticles.add(article);
+      }
+    }
+    setState(() {
+      articles = fetchedArticles;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Articles"),
+      ),
+      backgroundColor: Color(0xFFb91c1b),
+      body: Column(
+        children: [
+          const SizedBox(height: 10),
+          Center(
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const ArticleEntryForm(),
+                  ),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF2463EB),
+                foregroundColor: Colors.white,
+              ),
+              child: const Text("Add Article"),
+            ),
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    showAllArticles = true;
+                    _fetchArticlesFuture = fetchArticles(request);
+                  });
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: showAllArticles
+                      ? Theme.of(context).colorScheme.primary
+                      : Colors.white,
+                  foregroundColor: showAllArticles
+                      ? Colors.white
+                      : Theme.of(context).colorScheme.primary,
+                ),
+                child: const Text("Explore"),
+              ),
+              const SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    showAllArticles = false;
+                    _fetchArticlesFuture = fetchArticles(request);
+                  });
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: !showAllArticles
+                      ? Theme.of(context).colorScheme.primary
+                      : Colors.white,
+                  foregroundColor: !showAllArticles
+                      ? Colors.white
+                      : Theme.of(context).colorScheme.primary,
+                ),
+                child: const Text("My Articles"),
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
+          Expanded(
+            child: FutureBuilder(
+              future: _fetchArticlesFuture,
+              builder: (context, AsyncSnapshot snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (articles.isEmpty) {
+                  return const Center(
+                    child: Text("No articles found"),
+                  );
+                } else {
+                  return ListView.builder(
+                    itemCount: articles.length,
+                    itemBuilder: (context, index) {
+                      return ArticleCard(
+                        articles[index],
+                        showAllArticles,
+                        onDelete: () {
+                          setState(() {
+                            articles.removeAt(index);
+                          });
+                        },
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/article/article_card.dart
@@ -0,0 +1,147 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:oleh_bali_mobile/models/article_entry.dart';
+import 'package:oleh_bali_mobile/screens/article/edit_article.dart';
+import 'package:oleh_bali_mobile/screens/article/show_article.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+import 'package:provider/provider.dart';
+
+class ArticleCard extends StatelessWidget {
+  final ArticleEntry article;
+  final bool showAllArticles;
+  final VoidCallback onDelete; // Callback for delete action
+
+  const ArticleCard(this.article, this.showAllArticles, {required this.onDelete, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
+    return Card(
+      elevation: 4,
+      margin: const EdgeInsets.all(10),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(50),
+                  child: Image.network(
+                    article.user.profilepicture,
+                    width: 50,
+                    height: 50,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        article.user.displayname,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        article.user.nationality,
+                        style: const TextStyle(
+                          color: Colors.grey,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Text(
+                  article.time,
+                  style: const TextStyle(
+                    color: Colors.grey,
+                    fontSize: 12,
+                  ),
+                ),
+              ],
+            ),
+            Center(
+              child: Text(
+                article.title,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            if (article.img != null && article.img.isNotEmpty)
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  article.img,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            const SizedBox(height: 10),
+            Text(
+              article.text,
+              style: const TextStyle(
+                fontSize: 14,
+                height: 1.5,
+              ),
+            ),
+            if (!showAllArticles)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      // Implement edit functionality here
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => EditArticle(article: article,)),
+                      );
+                    },
+                    child: const Text('Edit'),
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      // Implement delete functionality here
+                      final response = await request.get(
+                        "http://localhost:8000/article/delete/mobile/${article.id}/",
+                      );
+
+                      if (response['status'] == 'success') {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text("Article deleted successfully."),
+                          ),
+                        );
+                        onDelete(); // Call the callback to update the parent widget
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text("Failed to delete article."),
+                          ),
+                        );
+                      }
+                    },
+                    child: const Text('Delete'),
+                  ),
+                ],
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
